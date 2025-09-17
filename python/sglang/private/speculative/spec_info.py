dbd8d301b99a0ed088310ce3dcd5d777798a5adb
Jue Wang <jue@together.ai>
2025-09-17 14:59:52 -0700

    fix spec enum

--- python/sglang/private/speculative/spec_info.py
+++ python/sglang/private/speculative/spec_info.py
@@ -1,26 +1,25 @@
 from enum import IntEnum, auto
 
-from sglang.srt.speculative.spec_info import SpeculativeAlgorithm as SGLANG_SpeculativeAlgorithm
 
 class SpeculativeAlgorithm(IntEnum):
 
-    NONE = SGLANG_SpeculativeAlgorithm.NONE
-    EAGLE = SGLANG_SpeculativeAlgorithm.EAGLE
-    EAGLE3 = SGLANG_SpeculativeAlgorithm.EAGLE3
-    STANDALONE = SGLANG_SpeculativeAlgorithm.STANDALONE
+    NONE = auto()
+    EAGLE = auto()
+    EAGLE3 = auto()
+    STANDALONE = auto()
     PHOENIX = auto()
         
     def is_none(self):
-        return self == SGLANG_SpeculativeAlgorithm.NONE
+        return self == SpeculativeAlgorithm.NONE
 
     def is_eagle(self):
-        return self == SGLANG_SpeculativeAlgorithm.EAGLE or self == SGLANG_SpeculativeAlgorithm.EAGLE3
+        return self == SpeculativeAlgorithm.EAGLE or self == SpeculativeAlgorithm.EAGLE3
 
     def is_eagle3(self):
-        return self == SGLANG_SpeculativeAlgorithm.EAGLE3
+        return self == SpeculativeAlgorithm.EAGLE3
 
     def is_standalone(self):
-        return self == SGLANG_SpeculativeAlgorithm.STANDALONE
+        return self == SpeculativeAlgorithm.STANDALONE
 
     def is_phoenix(self):
         return self == SpeculativeAlgorithm.PHOENIX
@@ -28,11 +27,11 @@ class SpeculativeAlgorithm(IntEnum):
     @staticmethod
     def from_string(name: str):
         name_map = {
-            "EAGLE": SGLANG_SpeculativeAlgorithm.EAGLE,
-            "EAGLE3": SGLANG_SpeculativeAlgorithm.EAGLE3,
+            "EAGLE": SpeculativeAlgorithm.EAGLE,
+            "EAGLE3": SpeculativeAlgorithm.EAGLE3,
             "PHOENIX": SpeculativeAlgorithm.PHOENIX,
-            "STANDALONE": SGLANG_SpeculativeAlgorithm.STANDALONE,
-            None: SGLANG_SpeculativeAlgorithm.NONE,
+            "STANDALONE": SpeculativeAlgorithm.STANDALONE,
+            None: SpeculativeAlgorithm.NONE,
         }
         if name is not None:
             name = name.upper()
