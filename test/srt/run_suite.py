7bb6fcaf4c77669eeb572effe885c8e2ed9f93b2
Yineng Zhang <me@zhyncs.com>
2025-12-10 13:05:27 -0800

    Copy OSS code from 12b7a4fa on 20251210 (#475)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- test/srt/run_suite.py
+++ test/srt/run_suite.py
@@ -23,8 +23,8 @@ suites = {
         TestFile("models/test_embedding_models.py", 73),
         TestFile("models/test_encoder_embedding_models.py", 221),
         TestFile("models/test_generation_models.py", 103),
-        # TestFile("models/test_nvidia_nemotron_nano_v2.py", 132),
-        # TestFile("models/test_nvidia_nemotron_nano_v2_vl.py", 214),  # GSM8k + MMMU
+        TestFile("models/test_nvidia_nemotron_nano_v2.py", 132),
+        TestFile("models/test_nvidia_nemotron_nano_v2_vl.py", 214),  # GSM8k + MMMU
         TestFile("models/test_qwen_models.py", 90),
         TestFile("models/test_reward_models.py", 103),
         TestFile("models/test_transformers_models.py", 245),
@@ -150,7 +150,7 @@ suites = {
         TestFile("test_gpt_oss_4gpu.py", 300),
         TestFile("test_local_attn.py", 411),
         TestFile("test_multi_instance_release_memory_occupation.py", 64),
-        TestFile("test_pp_single_node.py", 481),
+        TestFile("test_pp_single_node.py", 800),
         TestFile("test_piecewise_cuda_graph.py", 1200),
     ],
     "per-commit-8-gpu-h200": [
@@ -169,9 +169,10 @@ suites = {
     ],
     "per-commit-4-gpu-b200": [
         TestFile("test_deepseek_v3_fp4_4gpu.py", 1800),
-        TestFile("test_flash_attention_4.py", 300),
-        TestFile("test_gpt_oss_4gpu.py", 600),
-        TestFile("test_llama31_fp4.py", 300),
+        TestFile("test_flash_attention_4.py", 90),
+        TestFile("test_fp8_blockwise_gemm.py", 280),
+        TestFile("test_gpt_oss_4gpu.py", 700),
+        TestFile("test_llama31_fp4.py", 90),
         TestFile("test_eagle_infer_beta_dp_attention.py", 300),
     ],
     # "per-commit-8-gpu-b200": [
@@ -191,6 +192,7 @@ suites = {
     ],
     "quantization_test": [
         TestFile("quant/test_awq.py", 163),
+        TestFile("quant/test_marlin_moe.py", 200),
         TestFile("test_bnb.py", 5),
         TestFile("test_gptqmodel_dynamic.py", 102),
         TestFile("test_quantization.py", 185),
@@ -199,6 +201,7 @@ suites = {
     # Nightly test suites have been moved to test/run_suite_nightly.py
     "__not_in_ci__": [
         TestFile("test_release_memory_occupation.py", 200),  # Temporarily disabled
+        TestFile("lora/test_lora_hf_sgl_logprob_diff.py"),  # Nightly test
         TestFile("models/test_dummy_grok_models.py"),
         TestFile(
             "rl/test_update_weights_from_disk.py"
