5454e5f4485532190089c4012e15d931a1223600
root <root@research-dev-b200-01.cloud.together.ai>
2026-01-27 22:25:07 +0000

    port matching

--- scripts/pd/run_router.sh
+++ scripts/pd/run_router.sh
@@ -5,10 +5,8 @@ export HOST_IP=$(ifconfig ens10f0np0 | awk '/inet / {print $2}')
 
 python3 -m sglang_router.launch_router \
   --pd-disaggregation \
-  --prefill http://10.168.9.69:12347 \
-  --decode http://10.168.9.69:12348  \
+  --prefill http://10.168.9.72:12349 \
+  --decode http://10.168.9.72:12350 \
   --host ${HOST_IP} \
-  --port 8000 \
+  --port 8091 \
   --prefill-policy cache_aware
-
-  
